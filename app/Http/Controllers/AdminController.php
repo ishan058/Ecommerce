c832eba8db18e251852dcf0251c0e063556ef049
ishan058 <ishankhadkaik246@gmail.com>
2023-05-25 11:32:54 +0545

    error solved

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,7 +27,7 @@ class AdminController extends Controller
     
     }
 
-    public function delete_catagoty($id)
+    public function delete_catagory($id)
     {
     $data=catagory::find($id);
     $data->delete();
